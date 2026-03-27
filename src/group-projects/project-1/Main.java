e4f9b33c357b77fa19589ca591b1a350009420c4
Miles <maether@life365.health>
2026-03-26 21:00:19 -0600

    Header created. Will need to add Frank's last name to Authors section.

--- src/group-projects/project-1/Main.java
+++ src/group-projects/project-1/Main.java
@@ -1,3 +1,13 @@
+/**
+ * Assignment: Group Project 1
+ * Class: CS 361
+ * Authors: Miles Aether, Frank, Francisco
+ * Description: Part 1 implements a merge sort that splits the array into three subarrays then sorts each
+ *              subarray recursively. Part 2 uses a datasest from haveibeenpwned.com to hash, insert into
+ *              a bit array using insert logic that will allow the measure of false positives in terms of
+ *              finding that password from the dataset.
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
